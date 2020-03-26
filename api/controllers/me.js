d6cfa00e586438d2312c99a0e58a177c49d1c2d7
dynk <raphael.dynk@gmail.com>
2020-03-26 21:37:24 +0100

    fixed datapoint me

--- api/controllers/me.js
+++ api/controllers/me.js
@@ -16,7 +16,6 @@ module.exports = ({ mongoose }) => {
       try {
         const token = req.swagger.params['x-auth-token'].value
         const user = await UserModel.findByToken(token)
-        // const user = await UserModel.findByToken({ _id })
         res.status(200).json(user.toJSON())
         next()
       } catch (e) {
@@ -36,8 +35,10 @@ module.exports = ({ mongoose }) => {
     },
     mePut: async (req, res, next) => {
       try {
+        const token = req.swagger.params['x-auth-token'].value
+        const { _id: id } = await UserModel.findByToken(token)
         const body = req.swagger.params.body.value
-        const user = await UserModel.update(body)
+        const user = await UserModel.update(id, body)
         res.status(201).json(user.toJSON())
         next()
       } catch (e) {
@@ -46,10 +47,22 @@ module.exports = ({ mongoose }) => {
     },
     mePostDatapoint: async (req, res, next) => {
       try {
-        const user_id = req.swagger.params.userId.value
+        const token = req.swagger.params['x-auth-token'].value
+        const { _id: user_id } = await UserModel.findByToken(token)
         const body = req.swagger.params.body.value
         const datapoint = await DatapointModel.create({ user_id, ...body })
         res.status(201).json(datapoint.toJSON())
+        next()
+      } catch (e) {
+        next(e)
+      }
+    },
+    meGetDatapoints: async (req, res, next) => {
+      try {
+        const token = req.swagger.params['x-auth-token'].value
+        const { _id: user_id } = await UserModel.findByToken(token)
+        const datapoints = await DatapointModel.find({ user_id })
+        res.status(201).json(datapoints.map(d => d.toJSON()))
         next()
       } catch (e) {
         next(e)
